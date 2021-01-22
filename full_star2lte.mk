cda9468e447249809f85db7a0e307e5c025e6f1c
Julian Fölsch <julianf@paktosan.de>
2021-01-22 17:30:02 +0100

    Add necessary changes for Halium

--- full_star2lte.mk
+++ full_star2lte.mk
@@ -4,7 +4,8 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/halium.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/star2lte/device.mk)
